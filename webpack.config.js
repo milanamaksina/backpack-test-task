38d7100709b14c0b526078f0f89ed59684103f5e
Milana <m_maxinaa@mail.ru>
2025-07-30 00:22:12 +0300

    feat: project setup

---
+++ webpack.config.js
@@ -0,0 +1,89 @@
+const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const isProd = process.env.NODE_ENV === "production";
+
+module.exports = {
+  mode: isProd ? "production" : "development",
+
+  entry: "./src/main.ts",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: isProd ? "[name].[contenthash].js" : "[name].js",
+    clean: true,
+  },
+
+  resolve: {
+    extensions: [".ts", ".js", ".vue"],
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+
+  devtool: isProd ? false : "source-map",
+
+  devServer: {
+    hot: true,
+    open: true,
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    compress: true,
+    port: 3000,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        options: {
+          appendTsSuffixTo: [/\.vue$/],
+          transpileOnly: true,
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "vue-style-loader",
+          "css-loader",
+          "postcss-loader",
+          {
+            loader: "sass-loader",
+            options: {},
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: ["vue-style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.svg$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+};
