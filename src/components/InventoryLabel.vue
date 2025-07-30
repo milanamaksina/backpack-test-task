b2a4236925659535183476d9fd70bb873a8ea776
Milana <m_maxinaa@mail.ru>
2025-07-30 16:48:17 +0300

    feat: added inventory label

--- src/components/InventoryLabel.vue
+++ src/components/InventoryLabel.vue
@@ -1,5 +1,18 @@
 <template>
-  <div></div>
+  <div class="inventory-label">
+    <span class="label">{{ label }}</span>
+    <span class="item-counter">{{ `${totalItems}/100` }}</span>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script setup lang="ts">
+interface Props {
+  label: string;
+  totalItems: number;
+}
+defineProps<Props>();
+</script>
+
+<style lang="scss" scoped>
+@use "@/styles/components/inventory-label.scss";
+</style>
