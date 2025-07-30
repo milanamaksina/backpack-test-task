2982ff47ef8958d1d9d1f03cd0ab49d4cb0b36c9
Milana <m_maxinaa@mail.ru>
2025-07-30 17:21:49 +0300

    feat: added inventory tabs

--- src/components/InventoryTabs.vue
+++ src/components/InventoryTabs.vue
@@ -1,5 +1,17 @@
 <template>
-  <div></div>
+  <div class="tabs">
+    <button class="backpack-tab">BACKPACK</button>
+    <button class="nexus-tab">NEXUS</button>
+    <button class="close-btn">
+      <Icon name="close" class="close-icon" />
+    </button>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script setup>
+import Icon from "@/components/Icon.vue";
+</script>
+
+<style lang="scss" scoped>
+@use "@/styles/components/inventory-tabs.scss";
+</style>
