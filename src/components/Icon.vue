53e0bc9b4b37f844b56464cf0077d412678822cf
Milana <m_maxinaa@mail.ru>
2025-07-30 12:04:44 +0300

    feat: added sprite and icon component

---
+++ src/components/Icon.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg style="max-height: 56px">
+    <use :xlink:href="`/sprite.svg#icon-${name}`" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  name: { type: String, required: true },
+});
+</script>
