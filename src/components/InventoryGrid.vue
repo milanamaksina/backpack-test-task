421c354279ab750a644b6173b1e93ce31e12fb02
Milana <m_maxinaa@mail.ru>
2025-07-30 12:08:57 +0300

    feat: added inventory page and grid

---
+++ src/components/InventoryGrid.vue
@@ -0,0 +1,20 @@
+div
+<template>
+  <div class="inventory-grid-container">
+    <div class="inventory-grid">
+      <div v-for="index in totalSlots" :key="index" class="inventory-cell">
+        <InventoryCard />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import InventoryCard from "@/components/InventoryCard.vue";
+
+const totalSlots = 40;
+</script>
+
+<style lang="scss" scoped>
+@use "@/styles/components/inventory-grid.scss";
+</style>
---
+++ src/views/InventoryPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="inventory-page">
+    <InventoryTabs />
+    <div class="inventory-wrapper">
+      <InventoryFilter class="filter" />
+      <div class="inventory-backpack">
+        <InventoryLabel />
+        <InventoryGrid />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import InventoryGrid from "@/components/InventoryGrid.vue";
+import InventoryFilter from "@/components/InventoryFilter.vue";
+import InventoryTabs from "@/components/InventoryTabs.vue";
+import InventoryLabel from "@/components/InventoryLabel.vue";
+</script>
+<style lang="scss" scoped>
+@use "@/styles/views/inventory-page.scss";
+</style>
