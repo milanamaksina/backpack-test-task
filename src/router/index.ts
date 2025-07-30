ad3b32f48690d568faf5eb39731537ff73b8f58d
Milana <m_maxinaa@mail.ru>
2025-07-30 14:48:31 +0300

    feat: added case routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,25 @@
 import { createRouter, createWebHistory } from "vue-router";
 import InventoryPage from "@/views/InventoryPage.vue";
 
-const routes = [{ path: "/", name: "Inventory", component: InventoryPage }];
-
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes: [
+    {
+      path: "/",
+      name: "Inventory",
+      component: InventoryPage,
+      beforeEnter: (to) => {
+        if (!to.query.case || isNaN(Number(to.query.case))) {
+          return {
+            ...to,
+            query: { ...to.query, case: "1" },
+            replace: true,
+          };
+        }
+        return true;
+      },
+    },
+  ],
 });
 
 export default router;
--- src/views/InventoryPage.vue
+++ src/views/InventoryPage.vue
@@ -2,21 +2,65 @@
   <div class="inventory-page">
     <InventoryTabs />
     <div class="inventory-wrapper">
-      <InventoryFilter class="filter" />
+      <InventoryFilter />
       <div class="inventory-backpack">
         <InventoryLabel />
-        <InventoryGrid />
+        <InventoryGrid
+          :items="filteredItems"
+          :errorMessage="errorMessage"
+          :isLoading="isLoading"
+        />
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
+import { ref, computed, watch } from "vue";
+import { useRoute } from "vue-router";
+import { fetchInventory } from "../api/inventory";
 import InventoryGrid from "@/components/InventoryGrid.vue";
 import InventoryFilter from "@/components/InventoryFilter.vue";
 import InventoryTabs from "@/components/InventoryTabs.vue";
 import InventoryLabel from "@/components/InventoryLabel.vue";
+import type { InventoryItem } from "@/types/inventory";
+
+const route = useRoute();
+const selectedFilter = ref("all");
+const inventory = ref<InventoryItem[]>([]);
+const errorMessage = ref<string | null>(null);
+const isLoading = ref(false);
+
+const loadInventory = async (caseNumber: number) => {
+  try {
+    isLoading.value = true;
+    const data = await fetchInventory(caseNumber);
+    inventory.value = data.inventory;
+  } catch (error) {
+    errorMessage.value = `Error loading inventory: ${error}`;
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+watch(
+  () => route.query.case,
+  (newCase) => {
+    if (newCase && !isNaN(Number(newCase))) {
+      loadInventory(Number(newCase));
+    }
+  },
+  { immediate: true }
+);
+
+const filteredItems = computed(() => {
+  if (selectedFilter.value === "all") return inventory.value;
+  return inventory.value.filter(
+    (item: InventoryItem) => item.type === selectedFilter.value
+  );
+});
 </script>
+
 <style lang="scss" scoped>
 @use "@/styles/views/inventory-page.scss";
 </style>
