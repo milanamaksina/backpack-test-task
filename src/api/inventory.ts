56e885e1000c5bf47986f34cb9b050aad5b5fdaf
Milana <100792768+milanamaksina@users.noreply.github.com>
2025-07-31 15:50:56 +0300

    fix: fixed error language and added base and endpoint url

--- src/api/inventory.ts
+++ src/api/inventory.ts
@@ -1,10 +1,14 @@
+const BASE_URL =
+  "https://us-central1-seven-seven-bit-inhouse-helper.cloudfunctions.net";
+const INVENTORY_ENDPOINT = "/vueDevTestTask-getInventoryState";
+
 export async function fetchInventory(caseNumber: number) {
   const response = await fetch(
-    `https://us-central1-seven-seven-bit-inhouse-helper.cloudfunctions.net/vueDevTestTask-getInventoryState?case=${caseNumber}`
+    `${BASE_URL}${INVENTORY_ENDPOINT}?case=${caseNumber}`
   );
 
   if (!response.ok) {
-    throw new Error("Ошибка загрузки инвентаря");
+    throw new Error("Failed to load inventory");
   }
 
   return response.json();
